c336d4a5618068198e28c7c3d08a01239d80a496
alvaro-isern <sss12345iiialvaro@gmail.com>
2025-01-11 22:43:21 -0500

    Add funcion de busqueda

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        ConsultaPelicula consulta = new ConsultaPelicula();
+        System.out.println("Escriba el número de la película de Star Wars que quiere consultar: ");
+        try {
+            var numeroDePelicula = Integer.valueOf(teclado.nextLine());
+            Pelicula pelicula = consulta.busquedaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+            generador.guardarJson(pelicula);
+        } catch (NumberFormatException e){
+            System.out.println("Número no encontrado " + e.getMessage());
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación");
+        }
+    }
 }
